b9ce1c71a21754630705d75582f91ed779e5a8a1
dunnodunca <135488785+dunnodunca@users.noreply.github.com>
2023-06-06 08:48:57 -0400

    Add files via upload

---
+++ CompilerA12/codeA12/Compilers.h
@@ -0,0 +1,122 @@
+/*
+************************************************************
+* COMPILERS COURSE - Algonquin College
+* Code version: Summer, 2023
+* Author: TO_DO
+* Professors: Paulo Sousa
+************************************************************
+###################################################
+#                                                 #
+#    ALGONQUIN         @@@@@@@         COLLEGE    #
+#                  @@-----------@@                #
+#               @@@@|  M O L D  |@@@@             #
+#            @@@@@@@@-----------@@@@@@@@          #
+#         @@@@@@@@@@@@@  @@@@@@@   @@@@@@@        #
+#       @@@@@@@@@@@@@      @@@       @@@@@@       #
+#     @@@@@@@    @@@@@    @@@@       @@@@@@@@     #
+#    @@@@@@@       @@@@@ @@@@@@@    @@@@@@@@@@    #
+#   @@@@@@@        @@@@@ @@@@@ @@@@@@    @@@@@@   #
+#  @@@@@@@@@@    @@             @@@@      @@@@@@  #
+#  @@@@@@@@@@@@@@@  @@@@@  @@@@  @@@@   @@    @@  #
+# @@@@@@@@@@@@@@@   @@@@@ @@@@@   @@@@@@@@@    @@ #
+# @@@@@      @@@@   @@@ @@@ @@@   @@@@    @@@@@@@ #
+# @@@@        @@@@  @@@ @@@ @@@   @@@      @@@@@@ #
+#  @@@@     @@@@@@@              @@@@@    @@@@@@  #
+#  @@@@@@@@@@@     @@@  @@@   @@@    @@@@@@@@@@   #
+#   @@@@@@@@@@@   @@@ @@@@@@ @@@@@    @@@@@@@@@   #
+#    @@@@@@@@@@@@@@@ @@@@@@    @@@@@@@@@@@@@@@    #
+#     @@@@@@@@@       @@@        @@@@@@@@@@@      #
+#       @@@@@@         @@         @@@@@@@@@       #
+#         @@@@@       @@@@@     @@@@@@@@@         #
+#            @@@@@@@@@@@@@@@@@@@@@@@@@            #
+#               @@@@@@@@@@@@@@@@@@@               #
+#  COMPILERS        @@@@@@@@@@@        2023-S     #
+#                                                 #
+###################################################
+*/
+
+/*
+************************************************************
+* File name: Compilers.h
+* Compiler: MS Visual Studio 2022
+* Course: CST 8152 � Compilers, Lab Section: [011, 012]
+* Assignment: A12, A22, A32.
+* Date: May 01 2023
+* Professor: Paulo Sousa
+* Purpose: This file defines the functions called by main function.
+* Function list: mainReader(), mainScanner(), mainParser().
+*************************************************************/
+
+#ifndef COMPILERS_H_
+#define COMPILERS_H_
+
+#define DEBUG 0
+
+/*
+ * ............................................................................
+ * ADVICE 1:
+ * Please check the "TODO" labels to develop your activity.
+ *
+ * ADVICE 2: This file must be updated according to each assignment
+ * (see the progression: reader > scanner > parser).
+ * ............................................................................
+ */
+
+/* TO_DO: Adjust your language (cannot be "kao") */
+
+/* Language name */
+#define STR_LANGNAME	"Kao"
+
+/* Logical constants - adapt for your language */
+#define kao_TRUE  1
+#define kao_FALSE 0
+
+/*
+------------------------------------------------------------
+Data types definitions
+NOTE: Some types may not be directly used by your language,
+		but they can be necessary for conversions.
+------------------------------------------------------------
+*/
+
+/* TO_DO: Define your typedefs */
+typedef char			kao_char;
+typedef char*			kao_string;
+typedef int				kao_intg;
+typedef float			kao_real;
+typedef void			kao_void;
+
+typedef unsigned char	kao_boln;
+typedef unsigned char	kao_byte;
+
+typedef long			kao_long;
+typedef double			kao_doub;
+
+/*
+------------------------------------------------------------
+Programs:
+1: Reader - invokes MainReader code
+2: Scanner - invokes MainScanner code
+3: Parser - invokes MainParser code
+------------------------------------------------------------
+*/
+enum PROGRAMS {
+	PGM_READER	= 'R',
+	PGM_SCANNER = 'S',
+	PGM_PARSER	= 'P'
+};
+
+/*
+------------------------------------------------------------
+Main functions signatures
+(Code will be updated during next assignments)
+------------------------------------------------------------
+*/
+kao_intg mainReader(kao_intg argc, kao_string* argv);
+/* TO_DO: Remove comment for next assignmens: Scanner: A22 and Parser: A32
+kao_intg mainScanner(kao_intg argc, kao_string* argv);
+kao_intg mainParser(kao_intg argc, kao_string* argv);
+*/
+kao_void printLogo();
+
+#endif
